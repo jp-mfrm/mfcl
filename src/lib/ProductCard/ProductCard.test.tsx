3c4c22965e1b5b83c374704338b1599ae0bd72f3
Lee Collins <114224325@digitalmacboo18.local>
2020-08-24 14:57:44 -0500

    updated testing

--- src/lib/ProductCard/ProductCard.test.tsx
+++ src/lib/ProductCard/ProductCard.test.tsx
@@ -3,40 +3,53 @@ import { render } from '@testing-library/react'
 
 import ProductCard from './index'
 
-const defaultProps = ({
-  brand = '',
-  brandImg = '',
-  productImg = '',
-  title = '',
-  size = '',
-  discountedPrice = 0,
-  price = 0,
-  rating = 1,
-  reviews = 1,
-  matchPercentage = 100
-}) => ({
-  brand,
-  brandImg,
-  productImg,
-  title,
-  size,
-  discountedPrice,
-  price,
-  rating,
-  reviews,
-  matchPercentage
-})
+// const defaultProps = ({
+//   brand = '',
+//   brandImg = '',
+//   productImg = '',
+//   title = '',
+//   size = '',
+//   discountedPrice = 0,
+//   price = 100,
+//   rating = 1,
+//   reviews = 1,
+//   matchPercentage = 0
+// }) => ({
+//   brand,
+//   brandImg,
+//   productImg,
+//   title,
+//   size,
+//   discountedPrice,
+//   price,
+//   rating,
+//   reviews,
+//   matchPercentage
+// })
 
 describe('ProductCard Component', () => {
+  const defaultProps = {
+    brand: '',
+    brandImg: '',
+    productImg: '',
+    title: '',
+    size: '',
+    discountedPrice: 0,
+    price: 0,
+    rating: 1,
+    reviews: 1,
+    matchPercentage: 50
+  }
+
   it('should render the discounted price', () => {
-    const props = defaultProps({ discountedPrice: 100 })
-    const { container } = render(<ProductCard {...props} />)
-    expect(container.querySelector('.product-card-container')?.classList).toContain('price-cut')
+    // const props = defaultProps({ discountedPrice: 100 })
+    const { container } = render(<ProductCard {...defaultProps} discountedPrice={200} />)
+    expect(container.querySelector('.price-container > div > p')?.classList).toContain('discounted-price')
   })
 
   it('should render the price', () => {
-    const props = defaultProps({ price: 100 })
-    const { container } = render(<ProductCard {...props} />)
-    expect(container.querySelector('.product-card-container')?.classList).toContain('original-price')
+    // const props = defaultProps({ price: 100 })
+    const { container } = render(<ProductCard {...defaultProps} price={100} />)
+    expect(container.querySelector('.price-container > div > p')?.classList).toContain('original-price')
   })
 })
