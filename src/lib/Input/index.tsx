3034b73d280120b4ce5aaa4c4af4ca65e80c52a2
Mohammad Mustafa <mohammmu@Mohammads-MacBook-Pro.local>
2020-08-26 22:01:52 -0500

    Added button, sizes, info message. Adjust sass.

--- src/lib/Input/index.tsx
+++ src/lib/Input/index.tsx
@@ -4,52 +4,80 @@ import React, {
   ReactNode
 } from 'react';
 
+import clsx from 'clsx'
+import Button from '../Button'
+
 import styles from './input.module.scss';
 
 /* TODO: Dark mode input */
-
-/* TODO: hideBtn - boolean prop to show / hide button */
-/* TODO: buttonLabel - button label  */
 /* TODO: successMsg - submission success message */
 /* TODO: errorMsg - submission error message */
-/* TODO: disable - disable the input  */
-/* TODO: Q: Add input validation, depends on type */
 /* TODO: Q: Does textarea come as a variation from the Input component?  */
 /* TODO: > Should textarea be its own component?  */
 /* TODO: > How does textarea button look, does it with a button?  */
 
 export interface Props {
-  /* class to pass to the input wrapper: TODO make required */
-  inputType: 'text' | 'email' | 'password' | 'number'
-  /* class to pass to the input wrapper */
+  /** Class to pass to the input wrapper */
+  inputType: 'text' | 'email' | 'password' | 'number' | 'date'
+  /** Class to pass to the input wrapper */
   inputClass?: string
-  /* styles to pass to each section title wrapper */
+  /** Styles to pass to each section title wrapper */
   titleStyles?: CSSProperties
-  /* styles to pass to each section content wrapper */
+  /** Styles to pass to each section content wrapper */
   contentStyles?: CSSProperties
-  /* placeholder of the Input */
-  placeholder?: string;
-  /* placeholder of the Input */
-  
-  /* width of the Input */
-  width?: string
+  /** Placeholder of the Input */
+  placeholder: string
+  /** Option to show/hide button */
+  addBtn: boolean
+  /** Label for button */
+  btnLabel: string  
+  /** Size of the Input */
+  size: 'lg' | 'md' | 'sm'
+  /** Makes the input field required */
+  required: boolean
+  /** Makes the input field disabled */
+  disabled: boolean
+  /** Message to appear below the input field */
+  inputFooter?: string 
   [rest: string]: unknown; // WIP...rest property
 };
 
 const Input: FunctionComponent<Props> = ({
   inputType,
   inputClass,
-  titleStyles = {},
-  contentStyles = {},
+  titleStyles,
+  contentStyles,
   placeholder = '',
+  addBtn = false,
+  btnLabel = 'Submit',
+  size = 'lg',
+  required = false,
+  disabled = false,
+  inputFooter,
   ...rest
 }) => {
+  let inputField = [];
+
+  inputField.push(
+    <input className={clsx(styles['input'], styles[size], inputClass)} {...rest}
+      key="inputField"
+      type={inputType}
+      name="name"
+      required={required}
+      disabled={disabled}
+      placeholder={placeholder}/>
+  );
+  
+  if(addBtn) {
+    inputField.push(<Button key="inputBtn" disabled={disabled} type="submit">{btnLabel}</Button>);
+  } 
+
   return (
-    <div className={styles['input-wrapper']} {...rest}>
-      <input className={styles['input']} 
-        type="text" 
-        name="name" 
-        placeholder={placeholder}/>
+    <div className={clsx(styles['input-wrapper'])}>
+      <form className={clsx(styles['input-wrapper-inner'])} {...rest}>
+        {inputField}
+      </form>
+      <div className={clsx(styles['input-wrapper-footer'])}>{inputFooter}</div>
     </div>
   );
 };
