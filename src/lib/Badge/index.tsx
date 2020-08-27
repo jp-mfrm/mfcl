f9c71b8a08ec53109bdc1c41cbcb3eaef2d7f664
Lee Collins <114224325@digitalmacboo18.local>
2020-08-27 16:27:39 -0500

    added colors to badges

--- src/lib/Badge/index.tsx
+++ src/lib/Badge/index.tsx
@@ -5,16 +5,16 @@ import clsx from 'clsx'
 
 interface Props {
   children: string
-  color?: string
+  white?: string
   width?: string
   height?: string
   [rest: string]: unknown // ...rest property
 }
 
-const Badge: FunctionComponent<Props> = ({ children, width, height, color, ...rest }) => {
+const Badge: FunctionComponent<Props> = ({ children, width, height, white, ...rest }) => {
   return (
     <div className={styles['badge-wrapper']} {...rest}>
-      {children}
+      <p className={clsx(styles.badge, white && styles.whiteBadge)}>{children}</p>
     </div>
   )
 }
