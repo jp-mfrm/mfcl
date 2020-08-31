4bf119db29bda9c84b819ecbd096e01838daf039
Lee Collins <114224325@digitalmacboo18.local>
2020-08-31 12:16:25 -0500

    changed factory function and added props

--- src/lib/NumberIncrementer/factory.counter.test.tsx
+++ src/lib/NumberIncrementer/factory.counter.test.tsx
@@ -1,21 +1,14 @@
-import React from 'react'
+import { createFactoryCounter } from './factory.counter'
 import { render } from '@testing-library/react'
-
-import { createCounterSubtractFactory, createCounterAddFactory } from './factory.counter'
 
 describe('NumberIncrementer Component', () => {
   it('counts the numbers correctly', () => {
-    let num = 10
-    expect(createCounterAddFactory(num)).toEqual(11)
+    expect(createFactoryCounter(10, 'add')).toEqual(11)
   })
-
   it('equals 0 on the counter', () => {
-    let num = 0
-    expect(createCounterSubtractFactory(num)).toEqual(0)
+    expect(createFactoryCounter(1, 'subtract')).toEqual(0)
   })
-
   it('subtracts from the counter', () => {
-    let num = 3
-    expect(createCounterSubtractFactory(num)).toEqual(2)
+    expect(createFactoryCounter(3, 'subtract')).toEqual(2)
   })
 })
--- src/lib/NumberIncrementer/index.tsx
+++ src/lib/NumberIncrementer/index.tsx
@@ -1,34 +1,50 @@
 import React, { FunctionComponent, useState, useCallback } from 'react'
-import { createCounterSubtractFactory, createCounterAddFactory } from './factory.counter'
+import { createFactoryCounter } from './factory.counter'
 import clsx from 'clsx'
 
 import styles from './numberIncrementer.module.scss'
 
 interface Props {
+  name: string
+  label: string
+  showLabel?: boolean
   className?: string
+  onChange: () => void
   [rest: string]: unknown // ...rest property
 }
 
-const NumberIncrementer: FunctionComponent<Props> = ({ className, ...rest }) => {
+const NumberIncrementer: FunctionComponent<Props> = ({
+  name = 'Quantity',
+  label = 'Qty:',
+  showLabel,
+  onChange,
+  className,
+  ...rest
+}) => {
   const [number, setNumber] = useState(1)
 
   const subtractNumber = useCallback(() => {
-    setNumber(createCounterSubtractFactory(number))
+    setNumber(createFactoryCounter(number, 'subtract'))
   }, [number])
 
   const addNumber = useCallback(() => {
-    setNumber(createCounterAddFactory(number))
+    setNumber(createFactoryCounter(number, 'add'))
   }, [number])
 
   return (
     <div className={clsx(styles['number-incrementer-wrapper'], className)} {...rest}>
-      <button onClick={subtractNumber} data-testid="subtract" aria-label="subtract number">
-        &#8722;
-      </button>
-      <p data-testid="number">{number}</p>
-      <button onClick={addNumber} data-testid="add" aria-label="add number">
-        &#43;
-      </button>
+      <label className={clsx(styles.hidden, showLabel && styles['show-label'])} htmlFor={name}>
+        {label}
+      </label>
+      <div className={styles['number-incrementer']}>
+        <button onClick={subtractNumber} aria-label="Subtract Number">
+          &#8722;
+        </button>
+        <input type="text" readOnly value={number} name={name} />
+        <button onClick={addNumber} onChange={subtractNumber} aria-label="Add Number">
+          &#43;
+        </button>
+      </div>
     </div>
   )
 }
