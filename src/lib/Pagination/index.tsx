24640f1e12bc84f8244bcfa10a40c05313ed6ead
Lee Collins <114224325@digitalmacboo18.local>
2020-09-03 14:47:18 -0500

    updating pagination

--- src/lib/Pagination/index.tsx
+++ src/lib/Pagination/index.tsx
@@ -1,19 +1,41 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent } from 'react'
 
-import styles from './pagination.module.scss';
+import styles from './pagination.module.scss'
 
 interface Props {
-  [rest: string]: unknown; // ...rest property
-};
+  pageLimit: number
+  [rest: string]: unknown // ...rest property
+}
 
-const Pagination: FunctionComponent<Props> = ({
-  ...rest
-}) => {
+const Pagination: FunctionComponent<Props> = ({ ...rest }) => {
   return (
     <div className={styles['pagination-wrapper']} {...rest}>
-      Pagination
+      <TestSubject />
     </div>
-  );
-};
+  )
+}
 
-export default Pagination;
+export default Pagination
+
+const TestSubject: FunctionComponent = () => {
+  let numArr = Array.from(Array(51).keys())
+  let styles = {
+    width: '150px',
+    height: '150px',
+    border: 'solid black 1px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '15px 0'
+  }
+
+  const panelMap = numArr.map((number, i) => {
+    return (
+      <div style={styles} key={i}>
+        {number}
+      </div>
+    )
+  })
+
+  return <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap' }}>{panelMap}</div>
+}
