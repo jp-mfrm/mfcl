2515a5137789566edf1c249eeb5975e0d731674f
MoMustafa <shad.767@live.com>
2020-09-17 11:55:31 -0500

    Carousel: Added basic unit tests

--- src/Carousel/Carousel.test.tsx
+++ src/Carousel/Carousel.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import React, {ReactNode} from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
 
 import Carousel from './index';
-// import carouselHelper from './carouselHelper';
-import { Simulate } from 'react-dom/test-utils';
 
 describe('Carousel Component', () => {
 
@@ -11,50 +10,94 @@ describe('Carousel Component', () => {
     const { container } = render(
       <Carousel ariaLabel="test" carouselClass="test-class-name"></Carousel>
     );
-    expect(container.querySelector('.carousel-wrapper')?.classList).toContain('test-class-name')
+    expect(container.querySelector('.carousel-drag-wrapper')?.classList).toContain('test-class-name')
   })
 
   it('should align the buttons properly', () => {
     const { container, rerender } = render(
       <Carousel ariaLabel="test" infinite></Carousel>
     );
-    expect(container.querySelector('.carousel-wrapper-controls')?.classList).toContain('middle');
-    expect(container.querySelector('.carousel-wrapper-controls')?.classList).toContain('apart');
+    expect(container.querySelector('.carousel-drag-wrapper-control')?.classList).toContain('middle');
+    expect(container.querySelector('.carousel-drag-wrapper-control')?.classList).toContain('apart');
 
     rerender(
       <Carousel ariaLabel="test" autoSlide btnAlignment='top'></Carousel>
     );
-    expect(container.querySelector('.carousel-wrapper-controls')?.classList).toContain('top');
+    expect(container.querySelector('.carousel-drag-wrapper-control')?.classList).toContain('top');
 
     rerender(
       <Carousel ariaLabel="test" btnAlignment='top center'></Carousel>
     );
-    expect(container.querySelector('.carousel-wrapper-controls')?.classList).toContain('top');
-    expect(container.querySelector('.carousel-wrapper-controls')?.classList).toContain('center');
+    expect(container.querySelector('.carousel-drag-wrapper-control')?.classList).toContain('top');
+    expect(container.querySelector('.carousel-drag-wrapper-control')?.classList).toContain('center');
 
     rerender(
       <Carousel ariaLabel="test" btnAlignment='middle left bottom'></Carousel>
     );
-    expect(container.querySelector('.carousel-wrapper-controls')?.classList).toContain('middle');
-    expect(container.querySelector('.carousel-wrapper-controls')?.classList).toContain('left');
-    expect(container.querySelector('.carousel-wrapper-controls')?.classList).not.toContain('bottom');
+    expect(container.querySelector('.carousel-drag-wrapper-control')?.classList).toContain('middle');
+    expect(container.querySelector('.carousel-drag-wrapper-control')?.classList).toContain('left');
+    expect(container.querySelector('.carousel-drag-wrapper-control')?.classList).not.toContain('bottom');
   })
 
-  it('should trigger transition click events', () => {
-    const { container } = render (
-      <Carousel ariaLabel="test">
-        <div>one</div>
-        <div>two</div>
+  it('should call the control button functions', () => {
+    const { container } = render(
+      <Carousel draggable ariaLabel="test"></Carousel>
+    );
+
+    fireEvent.click(container.querySelector('.prev')!);
+    fireEvent.click(container.querySelector('.next')!);
+
+    //TO-DO: figure out how to check dom change
+
+  })
+
+  it('should hide the control buttons when necessary', () => {
+    const { container, rerender } = render(
+      <Carousel ariaLabel="test"></Carousel>
+    );
+
+    expect(container.querySelector('.prev')?.classList).not.toContain('hidden');
+    expect(container.querySelector('.next')?.classList).not.toContain('hidden');
+
+    rerender(<Carousel hideControls ariaLabel="test"></Carousel>);
+
+    expect(container.querySelector('.prev')?.classList).toContain('hidden');
+    expect(container.querySelector('.next')?.classList).toContain('hidden');
+
+  })
+
+  it('should autoslide when necessary', () => {
+    const { container } = render(
+      <Carousel autoSlide ariaLabel="test">
+        <div>One</div>
+        <div>Two</div>
+        <div>Three</div>
       </Carousel>
     );
-    fireEvent.click(container.querySelector('.prev')!, { button: 0 });
-    var childrenNodes = container.querySelectorAll('.carousel-wrapper-slider div')
-    expect(childrenNodes.length).toEqual(2);
-    expect(childrenNodes[0].innerHTML).toEqual('one');
-    expect(childrenNodes[1].innerHTML).toEqual('two');
 
-    fireEvent.click(container.querySelector('.next')!, { button: 0 });
-    childrenNodes = container.querySelectorAll('.carousel-wrapper-slider div')
-    expect(childrenNodes.length).toEqual(2);
+    //TO-DO: How to check if dom changed
+
   })
+
+  it('should trigger transition click events', async () => {
+    const { container } = render(
+      <Carousel infinite ariaLabel='test'>
+        <div>One</div>
+        <div>Two</div>
+        <div>Three</div>
+      </Carousel>
+    )
+    fireEvent.click(container.querySelector('.next')!);
+
+    screen.debug();
+  })
+
+  // it('should trigger transition click events', async () => {
+  //   var children: ReactNode;
+    
+  //   const { result } = renderHook(() => carouselHelper(children, 1, 1, 'middle apart', false, 'round', 3000, false, true, false));
+
+  //   console.log(result.current.alignment);
+
+  // })
 });
--- src/Carousel/index.tsx
+++ src/Carousel/index.tsx
@@ -6,7 +6,7 @@ import React, {
   cloneElement,
 } from 'react'
 
-import { carouselHelper, draggableHelper } from './carouselHelper'
+import { draggableHelper } from './carouselHelper'
 import clsx from 'clsx'
 import styles from './carousel.module.scss'
 
@@ -73,63 +73,60 @@ const Carousel: FunctionComponent<Props> = ({
 }) => {
 
   const {
-    slideFlexBasis,
-    childrenArr,
-    alignment,
-    sliderRef,
-    slideMargin,
+    draggableSliderRef,
+    sliderLeft,
+    slides,
+    sliderWidth,
     indicatorWrapper,
-    setAction,
-    handleTransitionEnd
-  } = carouselHelper(
+    alignment,
+    handleDragStart,
+    handleDragEndHandler,
+    handleDragActionHandler,
+    handleIndexCheck,
+    shiftSlide
+  } = draggableHelper(
     children,
     itemsToShow,
-    itemsToScroll,
+    marginless,
     btnAlignment,
     hideIndicators,
     indicatorStyle,
     duration,
-    marginless,
     infinite,
     autoSlide
   )
 
-  const slider = (
-    <div ref={sliderRef} className={clsx(styles['carousel-wrapper-slider'])} aria-live="off" onTransitionEnd={handleTransitionEnd}>
-      {childrenArr?.map((child: ReactNode, index: number) => {
-        if (isValidElement(child)) {
-          let label = `slide ${index+1} of ${childrenArr.length}`
-          return (
-            <div
-              key={index}
-              aria-label={label}
-              aria-hidden="true"
-              className={clsx(styles['slide'], marginless && styles['marginless'])}
-              style={{ ...child.props.style, flexBasis: `${slideFlexBasis}%`, margin: `0 ${slideMargin}%` }}
-            >
-              {cloneElement(child, child.props)}
-            </div>
-          )
-        }
-      })}
-    </div>
-  )
-
   const buttons = 
   <>
-    <button className={clsx(styles["prev"], styles["carousel-wrapper-controls"], hideControls && styles["hidden"], alignment)} onClick={() => setAction("prev")}
+    <button
+      id="prev"
       aria-hidden={hideControls && "true" || "false"}
+      className={clsx(
+        styles['carousel-drag-wrapper-control'],
+        styles['prev'],
+        hideControls && styles['hidden'],
+        alignment
+      )}
+      onClick={() => shiftSlide(-1)}
     >
       <p className={clsx(styles["sr-only"])}>
         Move Slider Left Button.
       </p>
     </button>
-    <button className={clsx(styles["next"], styles["carousel-wrapper-controls"], hideControls && styles["hidden"], alignment)} onClick={() => setAction("next")}
+    <button
+      id="next"
       aria-hidden={hideControls && "true" || "false"}
+      className={clsx(
+        styles['carousel-drag-wrapper-control'],
+        styles['next'],
+        hideControls && styles['hidden'],
+        alignment
+      )}
+      onClick={() => shiftSlide(1)}
     >
-    <p className={clsx(styles["sr-only"])}>
-      Move Slider Right Button. 
-    </p>
+      <p className={clsx(styles["sr-only"])}>
+        Move Slider Right Button.
+      </p>
     </button>
   </>
 
@@ -141,100 +138,32 @@ const Carousel: FunctionComponent<Props> = ({
     {!hideIndicators && 'You can also jump to a slide using the slide dots.'}
   </p>
 
-
-  var template
-
-  if (draggable) {
-    const {
-      draggableSliderRef,
-      sliderLeft,
-      slides,
-      sliderWidth,
-      indicatorWrapper,
-      alignment,
-      handleDragStart,
-      handleDragEndHandler,
-      handleDragActionHandler,
-      handleIndexCheck,
-      shiftSlide
-    } = draggableHelper(
-      children,
-      itemsToShow,
-      marginless,
-      btnAlignment,
-      hideIndicators,
-      indicatorStyle,
-      duration,
-      infinite,
-      autoSlide
-    )
-
-    template = (
-      <section className={clsx(styles['carousel-drag-wrapper'], styles['loaded'])} aria-label={'carousel-' + ariaLabel}>
-        {screenReaderInstructions}
-        <div className={styles['carousel-drag-wrapper-slider']}>
-          <div
-            className={styles['carousel-drag-wrapper-slides']}
-            aria-live="off"
-            style={{
-              left: `${sliderLeft}%`,
-              width: `${sliderWidth}%`
-            }}
-            ref={draggableSliderRef}
-            onMouseDown={handleDragStart}
-            onTouchStart={handleDragStart}
-            onTouchEnd={handleDragEndHandler}
-            onTouchMove={handleDragActionHandler}
-            onTransitionEnd={handleIndexCheck}
-          >
-            {slides}
-          </div>
-          {indicatorWrapper}
+  const template = (
+    <section className={clsx(styles['carousel-drag-wrapper'], styles['loaded'], carouselClass)} aria-label={'carousel-' + ariaLabel}>
+      {screenReaderInstructions}
+      <div className={styles['carousel-drag-wrapper-slider']}>
+        <div
+          className={styles['carousel-drag-wrapper-slides']}
+          aria-live="off"
+          style={{
+            left: `${sliderLeft}%`,
+            width: `${sliderWidth}%`
+          }}
+          ref={draggableSliderRef}
+          onMouseDown={handleDragStart}
+          onTouchStart={handleDragStart}
+          onTouchEnd={handleDragEndHandler}
+          onTouchMove={handleDragActionHandler}
+          onTransitionEnd={handleIndexCheck}
+        >
+          {slides}
         </div>
-        <button
-          id="prev"
-          aria-hidden={(hideControls && 'true') || 'false'}
-          className={clsx(
-            styles['carousel-drag-wrapper-control'],
-            styles['prev'],
-            hideControls && styles['hidden'],
-            alignment
-          )}
-          onClick={() => shiftSlide(-1)}
-        >
-        <p className={clsx(styles["sr-only"])}>
-          Move Slider Left Button.
-        </p>
-        </button>
-        <button
-          id="next"
-          aria-hidden={hideControls && "true" || "false"}
-          className={clsx(
-            styles['carousel-drag-wrapper-control'],
-            styles['next'],
-            hideControls && styles['hidden'],
-            alignment
-          )}
-          onClick={() => shiftSlide(1)}
-        >
-        <p className={clsx(styles["sr-only"])}>
-          Move Slider Right Button.
-        </p>
-        </button>
+        {indicatorWrapper}
+      </div>
+        {buttons}
       </section>
     )
-  } else {
-    template = (
-      <>
-        <section className={clsx(styles['carousel-wrapper'], carouselClass)} aria-label={'carousel-'+ariaLabel}>
-          {screenReaderInstructions}
-          {slider}
-          {indicatorWrapper}
-        </section>
-        {buttons}
-      </>
-    )
-  }
+  
   return template
 }
 
