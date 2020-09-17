f39f6ed7ea596a899c6a976274e72369a4c6a1a8
Lee Collins <114224325@digitalmacboo18.local>
2020-09-17 17:05:43 -0500

    added styles for desktop typography

--- src/Typography/index.tsx
+++ src/Typography/index.tsx
@@ -1,19 +1,71 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent } from 'react'
 
-import styles from './typography.module.scss';
+import styles from './typography.module.scss'
+import clsx from 'clsx'
 
 interface Props {
-  [rest: string]: unknown; // ...rest property
-};
+  children: string
+  variant:
+    | 'xxl'
+    | 'xl'
+    | 'lg'
+    | 'md'
+    | 'sm'
+    | 'xs'
+    | 'subtitle'
+    | 'body'
+    | 'body-sm'
+    | 'body-lg'
+    | 'price-lg'
+    | 'price'
+    | 'price-sale'
+  className?: string
+  [rest: string]: unknown // ...rest property
+}
 
-const Typography: FunctionComponent<Props> = ({
-  ...rest
-}) => {
+const Typography: FunctionComponent<Props> = ({ variant, children, className, ...rest }) => {
+  let text
+  if (variant === 'xxl') {
+    text = <h1 className={styles.xxl}>{children}</h1>
+  }
+  if (variant === 'xl') {
+    text = <h2 className={styles.xl}>{children}</h2>
+  }
+  if (variant === 'lg') {
+    text = <h3 className={styles.lg}>{children}</h3>
+  }
+  if (variant === 'md') {
+    text = <h4 className={styles.md}>{children}</h4>
+  }
+  if (variant === 'sm') {
+    text = <h5 className={styles.sm}>{children}</h5>
+  }
+  if (variant == 'xs') {
+    text = <h6 className={styles.xs}>{children}</h6>
+  }
+  if (variant === 'body') {
+    text = <p className={styles.body}>{children}</p>
+  }
+  if (variant === 'body-sm') {
+    text = <p className={styles['body-sm']}>{children}</p>
+  }
+  if (variant === 'body-lg') {
+    text = <p className={styles['body-lg']}>{children}</p>
+  }
+  if (variant === 'price-lg') {
+    text = <p className={styles['price-lg']}>{children}</p>
+  }
+  if (variant === 'price') {
+    text = <p className={styles.price}>{children}</p>
+  }
+  if (variant === 'price-sale') {
+    text = <p className={styles['price-sale']}>{children}</p>
+  }
   return (
-    <div className={styles['typography-wrapper']} {...rest}>
-      Typography
+    <div className={className} {...rest}>
+      {text}
     </div>
-  );
-};
+  )
+}
 
-export default Typography;
+export default Typography
