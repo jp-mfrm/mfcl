2cc39124eb18eef83540ffa8c257aa910f597b53
Lee Collins <114224325@digitalmacboo18.local>
2020-09-18 11:06:52 -0500

    aded to the docs and tests

--- src/Typography/Typography.test.tsx
+++ src/Typography/Typography.test.tsx
@@ -5,16 +5,25 @@ import Typography from './index'
 
 describe('Typography Component', () => {
   it('renders a custom tag', () => {
-    const { getByTestId } = render(
-      <Typography variant="xs" tag="p">
+    const { container } = render(
+      <Typography variant="xs" tag="h1">
         Hello World
       </Typography>
     )
-    expect(getByTestId('typography-wrapper').innerHTML).toContain('p')
+    expect(container.querySelector('.xs')?.outerHTML).toContain('h1')
   })
 
   it('renders a variant correctly', () => {
-    const { getByTestId } = render(<Typography variant="xs">Hello World</Typography>)
-    expect(getByTestId('typography-wrapper').querySelector('.xs')?.classList).toContain('xs')
+    const { container } = render(<Typography variant="xs">Hello World</Typography>)
+    expect(container.querySelector('.xs')).toBeInTheDocument
+  })
+
+  it('renders a className correctly', () => {
+    const { container } = render(
+      <Typography variant="xs" className="test-class-name">
+        Hello World
+      </Typography>
+    )
+    expect(container.querySelector('.xs')?.classList).toContain('test-class-name')
   })
 })
