655fe6355a4199e303e410d8d6fe24f4b2bd2744
Lee Collins <114224325@digitalmacboo18.local>
2020-09-18 11:49:28 -0500

    updated tests to 100%

--- src/Typography/Typography.test.tsx
+++ src/Typography/Typography.test.tsx
@@ -6,24 +6,24 @@ import Typography from './index'
 describe('Typography Component', () => {
   it('renders a custom tag', () => {
     const { container } = render(
-      <Typography variant="xs" tag="h1">
+      <Typography variant="h3" tag="h1">
         Hello World
       </Typography>
     )
-    expect(container.querySelector('.xs')?.outerHTML).toContain('h1')
+    expect(container.querySelector('.h3')?.outerHTML).toContain('h1')
   })
 
   it('renders a variant correctly', () => {
-    const { container } = render(<Typography variant="xs">Hello World</Typography>)
-    expect(container.querySelector('.xs')).toBeInTheDocument
+    const { container } = render(<Typography variant="h3">Hello World</Typography>)
+    expect(container.querySelector('.h3')).toBeInTheDocument
   })
 
   it('renders a className correctly', () => {
     const { container } = render(
-      <Typography variant="xs" className="test-class-name">
+      <Typography variant="h3" className="test-class-name">
         Hello World
       </Typography>
     )
-    expect(container.querySelector('.xs')?.classList).toContain('test-class-name')
+    expect(container.querySelector('.h3')?.classList).toContain('test-class-name')
   })
 })
