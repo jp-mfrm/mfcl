f2c8eae2ee669ee215b587ca3837042a6534ade1
Zoe <>
2020-09-24 15:28:08 -0400

    Fix mobile tab issues, take animation off position top tabs on mobile.

--- src/Tabs/Indicator.tsx
+++ src/Tabs/Indicator.tsx
@@ -1,59 +1,46 @@
-import React, { FunctionComponent, useState, useEffect } from 'react'
-
+import React, { FunctionComponent, useState, useEffect, useCallback } from 'react'
 import styles from './tabs.module.scss'
-
 interface Props {
   activeTabElement: any
   duration?: number
   position: 'top' | 'left'
 }
-
+const BREAKPOINT = 767
 const Indicator: FunctionComponent<Props> = ({ activeTabElement, position, duration }) => {
   const [style, setStyle] = useState<any>({})
-  const [width, setWidth] = React.useState(window.innerWidth);
-  const breakpoint = 767;
-  
-  const changeStyle = () => {
-    if (width > breakpoint) { 
-      if (activeTabElement?.current) {
-        const newStyle: any = {}
-        if (position === 'top') {
-          newStyle.bottom = '0px'
-          newStyle.left = activeTabElement.current.offsetLeft
-          newStyle.height = '5px'
-          newStyle.width = activeTabElement.current.offsetWidth
-        } else if (position === 'left') {
-          newStyle.right = '0px'
-          newStyle.top = activeTabElement.current.offsetTop
-          newStyle.height = activeTabElement.current.offsetHeight
-          newStyle.width = '5px'
-        }
-        setStyle(newStyle)
-      }
-    }
- 
-  }
+  const changeStyle = useCallback(() => {
+    const newStyle: any = {}
+ if (activeTabElement?.current) {
+   if (position === 'top' && window.innerWidth > BREAKPOINT) {
+     newStyle.bottom = '0px'
+     newStyle.left = activeTabElement.current.offsetLeft
+     newStyle.height = '5px'
+     newStyle.width = activeTabElement.current.offsetWidth
+   } else if (position === 'left') {
+     newStyle.right = '0px'
+     newStyle.top = activeTabElement.current.offsetTop
+     newStyle.height = activeTabElement.current.offsetHeight
+     newStyle.width = '5px'
+   }
+ }
 
+
+    setStyle(newStyle)
+  }, [activeTabElement, position])
 
   useEffect(() => {
     window.addEventListener('resize', changeStyle)
-
     return () => {
       window.removeEventListener('resize', changeStyle)
-
     }
-  }, [])
-
+  }, [changeStyle])
+  
   useEffect(() => {
-    setWidth(window.innerWidth)
     changeStyle()
   }, [activeTabElement, position])
-
   return <div className={styles.indicator} style={style} />
 }
-
 Indicator.defaultProps = {
   duration: 300
 }
-
 export default Indicator
