7e2b3e4b99e108ff02c8cf231e4f9f6df39d1625
Lee Collins <114224325@digitalmacboo18.local>
2020-09-29 11:47:19 -0500

    missed two files yay

--- src/Icons/Innerspring.tsx
+++ src/Icons/Innerspring.tsx
@@ -13,7 +13,7 @@ const Innerspring: React.FunctionComponent<Props> = ({
   fillColor = '#2D2926',
   ...rest
 }) => (
-  <svg width={width} height={height} {...rest} fill="none" viewBox="0 0 25 24">
+  <svg width={width} height={height} fill="none" viewBox="0 0 25 24" {...rest}>
     <mask id="6js2pvpm6a" width="25" height="25" x="0" y="0" maskUnits="userSpaceOnUse">
       <path
         fill="#fff"
--- src/Icons/Instagram.tsx
+++ src/Icons/Instagram.tsx
@@ -8,7 +8,7 @@ interface Props {
 }
 
 const Instagram: React.FunctionComponent<Props> = ({ width = '24', height = '24', fillColor = '#2D2926', ...rest }) => (
-  <svg width={width} height={height} {...rest} fill="none" viewBox="0 0 24 24">
+  <svg width={width} height={height} fill="none" viewBox="0 0 24 24" {...rest}>
     <mask id="52fh4xsyba" width="24" height="24" x="0" y="0" maskUnits="userSpaceOnUse">
       <path
         fill="#fff"
