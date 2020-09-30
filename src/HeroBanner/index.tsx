965b77e005df7ce3dac9cab939285fd5874e1045
Lee Collins <114224325@digitalmacboo18.local>
2020-09-30 15:05:05 -0500

    updated docs and rearranged props

--- src/HeroBanner/index.tsx
+++ src/HeroBanner/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, ReactNode, ReactChild } from 'react'
+import React, { FunctionComponent, ReactSVG } from 'react'
 import Typography from '../Typography'
 import Button from '../Button'
 import clsx from 'clsx'
@@ -6,16 +6,15 @@ import clsx from 'clsx'
 import styles from './heroBanner.module.scss'
 
 interface Props {
-  bgColor?: string
-  imageSrc?: string
-  imageAlt?: string
-  brandLogo?: ReactNode
   title: string
   body: string
+  imageSrc?: string
+  imageAlt?: string
+  brandLogo?: ReactSVG
   btnText?: string
   btnLink?: string
-  btnColor?: string
   fullSizeImg?: boolean
+  bgColor?: string
   bgSize?: 'contain' | 'repeat' | 'cover'
   textColor?: 'primary' | 'secondary' | 'error' | 'alert' | 'success' | 'white'
   className?: string
@@ -34,13 +33,11 @@ const HeroBanner: FunctionComponent<Props> = ({
   fullSizeImg,
   bgSize = 'cover',
   textColor = 'white',
-  btnColor = '#3a4367',
   className,
   ...rest
 }) => {
   let infoPanelStyles = { backgroundColor: `${bgColor}` }
   let heroBannerStyles = { background: `url(${imageSrc})`, backgroundSize: bgSize }
-  let btnStyles = { backgroundColor: btnColor, border: btnColor }
 
   let imageBg = fullSizeImg ? heroBannerStyles : infoPanelStyles
 
@@ -53,6 +50,7 @@ const HeroBanner: FunctionComponent<Props> = ({
     )
   }
 
+  //TODO: Get brand logo to render correctly
   let brandIcon
   if (brandLogo) {
     brandIcon = <span className={styles.brandLogo}>{brandLogo}</span>
@@ -61,7 +59,7 @@ const HeroBanner: FunctionComponent<Props> = ({
   let heroBtn
   if (btnText) {
     heroBtn = (
-      <Button type="button" className={styles.btnStyles} style={btnStyles} size="md" href={btnLink}>
+      <Button type="button" className={styles.btnStyles} size="md" href={btnLink}>
         {btnText}
       </Button>
     )
@@ -70,7 +68,7 @@ const HeroBanner: FunctionComponent<Props> = ({
   return (
     <div className={clsx(styles['hero-banner-wrapper'], className)} {...rest}>
       <div className={styles.infoPanelContainer} style={imageBg}>
-        {brandIcon}
+        {brandLogo}
         <Typography className={styles.title} variant="h4" color={textColor}>
           {title}
         </Typography>
