eb2915b2010951c3dd69f4a174cdaf63b8d8ebdf
MoMustafa <shad.767@live.com>
2020-09-30 21:53:16 -0500

    #7922: Added className to Ellipses Icon

--- src/Icons/Ellipses.tsx
+++ src/Icons/Ellipses.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 
 const Ellipses: React.FunctionComponent<Props> = ({ width = '92', height = '2', stroke = "#D63426", ...rest }) => (
-<svg width="92" height="2" viewBox="0 0 92 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg className={rest.className}  width="92" height="2" viewBox="0 0 92 2" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path d="M-8.74227e-08 1L92 0.999998" stroke={stroke} stroke-dasharray="2 5"/>
 </svg>
 
