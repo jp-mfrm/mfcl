96dc70539409f4fbf51797026a0cfccbaef242f9
MoMustafa <shad.767@live.com>
2020-09-30 21:51:47 -0500

    #7922: Added Shipping Status Icons

---
+++ src/Icons/Ellipses.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+interface Props {
+  width?: string | number
+  height?: string | number
+  className?: string
+  stroke ?: string
+  [x: string]: unknown // for the rest property
+}
+
+const Ellipses: React.FunctionComponent<Props> = ({ width = '92', height = '2', stroke = "#D63426", ...rest }) => (
+<svg width="92" height="2" viewBox="0 0 92 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M-8.74227e-08 1L92 0.999998" stroke={stroke} stroke-dasharray="2 5"/>
+</svg>
+
+)
+
+export default Ellipses
