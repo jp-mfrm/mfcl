d0358baa9ad3f7cba2d6943e308763269969ca43
Lee Collins <114224325@digitalmacboo18.local>
2020-10-16 16:49:21 -0500

    working on functionality

--- src/Textarea/index.tsx
+++ src/Textarea/index.tsx
@@ -1,5 +1,5 @@
-import React, { CSSProperties, FunctionComponent, ReactNode } from 'react'
-
+import React, { CSSProperties, forwardRef, useEffect, FunctionComponent, ReactNode, useState } from 'react'
+import useForwardedRef from '../utils/useForwardedRef'
 import clsx from 'clsx'
 import styles from './textarea.module.scss'
 
@@ -18,19 +18,25 @@ interface Props {
   textAreaMessage?: string
   /** Field and label name */
   name?: string
+  /** You already know what this is for. Why are you looking up the description? */
+  onChange?: Function
   [rest: string]: unknown // ...rest property
 }
 
-const Textarea: FunctionComponent<Props> = ({
-  className,
-  fieldStyling,
-  wrapperStyling,
-  error = false,
-  label,
-  name,
-  textAreaMessage,
-  ...rest
-}) => {
+const Textarea: FunctionComponent<Props> = forwardRef<HTMLInputElement, Props>(function TextField(props, ref) {
+  const {
+    className,
+    fieldStyling,
+    wrapperStyling,
+    error = false,
+    label,
+    name,
+    textAreaMessage,
+    onChange,
+    ...rest
+  } = props
+  const [hasValue, setHasValue] = useState(false)
+  const forwardedRef = useForwardedRef(ref)
   const errorClass = error && styles.error
 
   const handleKeyUp = (e: any) => {
@@ -60,25 +66,48 @@ const Textarea: FunctionComponent<Props> = ({
     window.scrollTo(0, scrollTop)
   }
 
+  const formControl = (e: any) => {
+    const length = e.target.value.length
+
+    // extra checks to prevent unnecessary rerenders every keystroke
+    if (hasValue && length === 0) {
+      setHasValue(false)
+    } else if (!hasValue && length > 0) {
+      setHasValue(true)
+    }
+
+    if (onChange) {
+      onChange(e)
+    }
+  }
+
+  useEffect(() => {
+    if ((forwardedRef as React.RefObject<HTMLInputElement>)?.current?.value) {
+      setHasValue(true)
+    }
+  }, [])
+
   return (
-    <div className={clsx(styles['textarea-wrapper'])} style={wrapperStyling}>
+    <div className={styles['textarea-wrapper']} style={wrapperStyling}>
       <div className={styles.inner}>
-        {label && (
-          <label htmlFor={name} className={styles.label}>
-            {label}
-          </label>
-        )}
         <textarea
-          className={clsx(styles.textarea, className, error && styles.error)}
+          className={clsx(styles.input, errorClass, hasValue && styles['has-value'], className)}
           name={name}
           onKeyDown={handleKeyDown}
           onKeyUp={handleKeyUp}
+          onChange={formControl}
+          ref={forwardedRef}
           {...rest}
         />
+        {label && (
+          <label htmlFor={name} className={clsx(styles.label, errorClass)}>
+            {label}
+          </label>
+        )}
       </div>
       {textAreaMessage && <p className={clsx(styles.footer, errorClass)}>{textAreaMessage}</p>}
     </div>
   )
-}
+})
 
 export default Textarea
