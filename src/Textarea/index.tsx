7771810dcd48f135842cd0b1cb1e43c3b7083abd
Gray754 <45702032+Gray754@users.noreply.github.com>
2020-10-19 14:40:18 -0500

    Textarea update (#70)
    * fixed styles to be consistent with input
    
    * working on functionality
    
    * functionality working
    
    * fixed styles and added additional documentation
    
    * updated props and fixed typo
    
    Co-authored-by: Lee Collins <114224325@digitalmacboo18.local>

--- src/Textarea/index.tsx
+++ src/Textarea/index.tsx
@@ -1,12 +1,12 @@
-import React, { CSSProperties, FunctionComponent, ReactNode } from 'react'
-
+import React, { CSSProperties, forwardRef, useEffect, FunctionComponent, ReactNode, useState } from 'react'
+import useForwardedRef from '../utils/useForwardedRef'
 import clsx from 'clsx'
 import styles from './textarea.module.scss'
 
 interface Props {
   /** Class to pass to the textarea wrapper */
   className?: string
-  /** Syles to pass to the textarea element */
+  /** Styles to pass to the textarea element */
   fieldStyling?: CSSProperties
   /** Styles to pass to the wrapper element */
   wrapperStyling?: CSSProperties
@@ -14,20 +14,31 @@ interface Props {
   error?: boolean
   /** Label for textarea field */
   label?: string | ReactNode
+  /** Message for input submission  */
+  textAreaMessage?: string
   /** Field and label name */
   name?: string
+  /** You already know what this is for. Why are you looking up the description? */
+  onChange?: Function
   [rest: string]: unknown // ...rest property
 }
 
-const Textarea: FunctionComponent<Props> = ({
-  className,
-  fieldStyling,
-  wrapperStyling,
-  error = false,
-  label,
-  name,
-  ...rest
-}) => {
+const Textarea: FunctionComponent<Props> = forwardRef<HTMLInputElement, Props>(function TextField(props, ref) {
+  const {
+    className,
+    fieldStyling,
+    wrapperStyling,
+    error = false,
+    label,
+    name,
+    textAreaMessage,
+    onChange,
+    ...rest
+  } = props
+  const [hasValue, setHasValue] = useState(false)
+  const forwardedRef = useForwardedRef(ref)
+  const errorClass = error && styles.error
+
   const handleKeyUp = (e: any) => {
     if (e.target.value.length > 0) return
     e.target.style.height = 'inherit'
@@ -55,22 +66,49 @@ const Textarea: FunctionComponent<Props> = ({
     window.scrollTo(0, scrollTop)
   }
 
+  const formControl = (e: any) => {
+    const length = e.target.value.length
+
+    // extra checks to prevent unnecessary rerenders every keystroke
+    if (hasValue && length === 0) {
+      setHasValue(false)
+    } else if (!hasValue && length > 0) {
+      setHasValue(true)
+    }
+
+    if (onChange) {
+      onChange(e)
+    }
+  }
+
+  useEffect(() => {
+    if ((forwardedRef as React.RefObject<HTMLInputElement>)?.current?.value) {
+      setHasValue(true)
+    }
+  }, [])
+
   return (
-    <div className={clsx(styles['textarea-wrapper'])} style={wrapperStyling}>
-      {label && (
-        <label htmlFor={name} className={styles.label}>
-          {label}
-        </label>
-      )}
-      <textarea
-        className={clsx(styles.textarea, className, error && styles.error)}
-        name={name}
-        onKeyDown={handleKeyDown}
-        onKeyUp={handleKeyUp}
-        {...rest}
-      />
+    <div className={styles['textarea-wrapper']} style={wrapperStyling}>
+      <div>
+        <textarea
+          className={clsx(styles.textarea, errorClass, hasValue && styles['has-value'], className)}
+          name={name}
+          onKeyDown={handleKeyDown}
+          onKeyUp={handleKeyUp}
+          onChange={formControl}
+          ref={forwardedRef}
+          style={fieldStyling}
+          {...rest}
+        />
+        {label && (
+          <label htmlFor={name} className={clsx(styles.label, errorClass)}>
+            {label}
+          </label>
+        )}
+      </div>
+      {textAreaMessage && <p className={clsx(styles.footer, errorClass)}>{textAreaMessage}</p>}
     </div>
   )
-}
+})
 
 export default Textarea
