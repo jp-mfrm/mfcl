87caf38956ff912c00b7841e0e0ad48040431b1a
Craig Walker <craigwalker1123@gmail.com>
2020-10-26 13:06:59 -0700

    Rewrote checkbox component

--- src/Checkbox/checkbox.test.tsx
+++ src/Checkbox/checkbox.test.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Checkbox from './index'
 
 describe('Checkbox Component', () => {
-  it('should render the props correctly', () => {
-    render(<Checkbox title="checkbox" />)
+  it('should render the wrapperClass prop', () => {
+    const { container } = render(<Checkbox wrapperClass="test-class" />)
+    expect(container.querySelector('.checkbox-container')?.classList).toContain('test-class')
+  })
+  it('should render the className prop', () => {
+    const { container } = render(<Checkbox className="test-class" />)
+    const checkbox = container.querySelector('.checkbox')
+    expect(checkbox?.classList).toContain('test-class')
+    // @ts-ignore
+    fireEvent.click(checkbox)
+  })
+  it('should render the label labelClass prop', () => {
+    const label = 'label'
+    const { container, getByText } = render(<Checkbox labelClass="test-class" label={label} />)
+    expect(container.querySelector('.label')?.classList).toContain('test-class')
+    expect(getByText(label)).toBeInTheDocument()
+  })
+  it('handles onChange', () => {
+    let result = false
+    const onChange = (e: any, newChecked: boolean) => {
+      result = newChecked
+    }
+    const { container } = render(<Checkbox onChange={onChange} />)
+    const checkbox = container.querySelector('.checkbox')
+    expect(result).toBe(false)
+    // @ts-ignore
+    fireEvent.click(checkbox)
+    expect(result).toBe(true)
   })
 })
