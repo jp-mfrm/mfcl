f5b4df6b07666e6dfc5dcb99de36c1a6f3de6299
Zoe <>
2020-11-06 15:27:15 -0500

    Clean up button and accordion.

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -51,7 +51,7 @@ const Button: FunctionComponent<Props> = forwardRef<unknown, Props>(function But
     </div>
   )
 
-  if (btnType === 'link') {
+  if (href) {
     return (
       // @ts-ignore
       <a href={href} className={btnClassName} ref={ref} {...rest}>
