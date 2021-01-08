5d83441b22efae7cdbc5260e4aaad0e0c8fb345e
Craig Walker <craigwalker1123@gmail.com>
2021-01-08 14:02:02 -0800

    testing chip

--- src/Chip/Chip.test.tsx
+++ src/Chip/Chip.test.tsx
@@ -1,13 +1,54 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
 
-import Chip from './index';
+import Chip from './index'
 
 describe('Chip Component', () => {
-  it('renders a className', () => {
-    const { container } = render(
-      <Chip className="test-class-name">Click Me</Chip>,
-    );
-    expect(container.querySelector('.chip-wrapper')?.classList).toContain('test-class-name')
+  it('renders all style props', () => {
+    const { container } = render(<Chip label="test" className="test-class-name" />)
+    expect(container.querySelector('.chip')?.classList).toContain('test-class-name')
   })
-});
+
+  it('renders a label', () => {
+    const { getByText } = render(<Chip label="test" />)
+    expect(getByText('test')).toBeInTheDocument()
+  })
+
+  it('renders the correct size', () => {
+    const { container } = render(<Chip label="test" size="sm" />)
+    expect(container.querySelector('.chip')?.classList).toContain('sm')
+  })
+
+  it('renders the correct variant', () => {
+    const { container } = render(<Chip label="test" variant="filled" />)
+    // @ts-ignore
+    fireEvent.click(chip) // won't delete
+    expect(container.querySelector('.chip')?.classList).toContain('filled')
+  })
+
+  it('can delete', () => {
+    let newLabel = ''
+    const onDelete = (label: string) => {
+      newLabel = label
+    }
+    const { container } = render(<Chip label="test" onDelete={onDelete} />)
+    const chip = container.querySelector('.chip')
+    // @ts-ignore
+    fireEvent.click(chip)
+    expect(newLabel).toBe('test')
+    newLabel = ''
+    // @ts-ignore
+    fireEvent.keyUp(chip, { key: 'Delete' })
+    expect(newLabel).toBe('test')
+    newLabel = ''
+    // @ts-ignore
+    fireEvent.keyUp(chip, { key: 'Backspace' })
+    expect(newLabel).toBe('test')
+    newLabel = ''
+    // @ts-ignore
+    fireEvent.keyUp(chip, { key: 'Enter' })
+    expect(newLabel).toBe('test')
+    // @ts-ignore
+    fireEvent.keyUp(chip, { key: 'Escape' })
+  })
+})
