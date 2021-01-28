159ba0a3ec3d6385895253f6be3d81a0917d977b
Carlos Rangel <carlos.rangel@sogeti.com>
2021-01-28 12:04:55 -0600

    Updated skeleton tests

--- src/Skeleton/Skeleton.test.tsx
+++ src/Skeleton/Skeleton.test.tsx
@@ -33,4 +33,36 @@ describe('Skeleton Component', () => {
     const { container } = render(<Skeleton circle />)
     expect(container.querySelector('.circle')).toBeInTheDocument()
   })
+
+  it('renders the skeleton with correct width and height units', () => {
+    const { container, rerender } = render(<Skeleton width="10" height="10" />)
+    expect(container.querySelector('.pulse')).toHaveStyle({
+      width: '10px',
+      height: '10px'
+    })
+
+    rerender(<Skeleton type="button-secondary" width={10} height={10} />)
+    expect(container.querySelector('.pulse')).toHaveStyle({
+      width: '10px',
+      height: '10px'
+    })
+
+    rerender(<Skeleton type="button-secondary" width="10em" height="10em" />)
+    expect(container.querySelector('.pulse')).toHaveStyle({
+      width: '10em',
+      height: '10em'
+    })
+
+    rerender(<Skeleton type="button-secondary" width="10%" height="10%" />)
+    expect(container.querySelector('.pulse')).toHaveStyle({
+      width: '10%',
+      height: '10%'
+    })
+
+    rerender(<Skeleton type="button-secondary" width="" height="" />)
+    expect(container.querySelector('.pulse')).toHaveStyle({
+      width: '',
+      height: ''
+    })
+  })
 })
