ba133b5a9d30c71ad1c38704de432f84ce0d2e1d
MoMustafa <shad.767@live.com>
2021-02-08 15:54:42 -0600

    added the cart icon

---
+++ src/Icons/Cart.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+interface Props {
+  width?: string | number
+  height?: string | number
+  fillColor?: string
+  fillOpacity?: string
+  stroke?: string
+  [x: string]: unknown // for the rest property
+}
+
+const Cart: React.FunctionComponent<Props> = ({ width = '37', height = '28', fillColor="white", fillOpacity="0.3", stroke="white", ...rest }) => (
+  <svg width={width} height={height} viewBox="0 0 37 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M8 15.6994C8 15.6994 17.7072 13.966 21.9971 12.7267C27.9387 11.0101 28.501 6.20176 31 4.30088C32.3305 3.28883 36.1706 3.27927 35.988 3.62509C34.8238 5.82881 32.1693 20.9996 32.1693 20.9996L9.4021 20.5614L8 15.6994Z"
+      fill={fillColor}
+      fill-opacity={fillOpacity}
+    />
+    <path
+      d="M1 1H5.47209L9.6483 20H31.9849L36 2.46224"
+      stroke={stroke}
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <circle cx="13.25" cy="25.25" r="2.25" fill={fillColor} />
+    <circle cx="28.25" cy="25.25" r="2.25" fill={fillColor} />
+  </svg>
+)
+
+export default Cart
