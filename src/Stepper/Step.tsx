688ad76a743420d1dac213f089f797da627def1a
Andrew Hahn <208hahn@gmail.com>
2021-12-21 10:22:30 -0700

    Reverted white space adjustment.

--- src/Stepper/Step.tsx
+++ src/Stepper/Step.tsx
@@ -1,4 +1,4 @@
-import React, {FunctionComponent, ReactNode, memo, KeyboardEvent} from 'react'
+import React, { FunctionComponent, ReactNode, memo, KeyboardEvent } from 'react'
 import clsx from 'clsx'
 
 import styles from './stepper.module.scss'
@@ -38,7 +38,7 @@ const Step: FunctionComponent<Props> = ({
   theNextActive,
   vertical
 }) => {
-  const {color = '#d63426', icon, label, className} = step
+  const { color = '#d63426', icon, label, className } = step
   const verticalClass = vertical && styles.vertical
   const currentOrPassedClass = currentOrPassed ? styles.passed : styles['not-passed']
   const activeStepClass = theNextActive && styles['next-active']
@@ -55,8 +55,8 @@ const Step: FunctionComponent<Props> = ({
         itemID={"step-" + index}
         style={
           activeStep
-            ? {border: `2px solid ${color}`}
-            : {border: `2px solid ${color}`, cursor: selectIndex ? 'pointer' : ''}
+            ? { border: `2px solid ${color}` }
+            : { border: `2px solid ${color}`, cursor: selectIndex ? 'pointer' : '' }
         }
       >
         {icon}
